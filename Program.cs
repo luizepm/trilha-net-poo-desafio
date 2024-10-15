bd39a189f45c5789eb6a0084fb0174a3ae83f9ec
luizepm <42193584+luizepm@users.noreply.github.com>
2024-10-14 23:52:49 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("11944444444", "N95", "1234567890", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("19955555555", "Iphone 14 Pro Max", "9876543210", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
