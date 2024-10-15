6ba2868c8de5a853c2e3c61794a9c5e79939ea85
luizepm <42193584+luizepm@users.noreply.github.com>
2024-10-14 23:54:31 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,6 +1,6 @@
 namespace DesafioPOO.Models
 {
-        public class Iphone
+    public class Iphone : Smartphone
     {
         public Iphone(string numero, string modelo, string imei, int memoria) : base(numero,  modelo, imei, memoria)
         {
